7ab81eb0965b60b4f20b77d2d5d2c2628233164b
Rizwan-Khan-TX <125319707+Rizwan-Khan-TX@users.noreply.github.com>
2023-02-14 08:58:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
+streamlit.title('My Parents New Healthy Diner')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach 3 & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free- Range Egg')
 
-streamlit.title('My Parents New Healthy Diner');
